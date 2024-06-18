981e8088fb6746fd24a2a9cc39122e7c521ef5d5
valued mammal <valuedmammal@protonmail.com>
2024-06-18 18:05:29 -0400

    example: remove wallet persistence
    and populate the watch list using
    `peek_address` instead of revealing scripts

--- examples/wallet.rs
+++ examples/wallet.rs
@@ -5,7 +5,6 @@ use std::net::{IpAddr, Ipv4Addr};
 use std::str::FromStr;
 
 use bdk_chain::bitcoin::{BlockHash, Network, ScriptBuf};
-use bdk_file_store::Store;
 use bdk_wallet::wallet;
 use bdk_wallet::KeychainKind;
 use bdk_wallet::Wallet;
@@ -13,53 +12,36 @@ use bdk_wallet::Wallet;
 use kyoto::chain::checkpoints::HeaderCheckpoint;
 use kyoto::node::builder::NodeBuilder;
 
-// Sync a bdk wallet
+/* Sync a bdk wallet */
+
+// Highest derivation index to include in the sync
+const TARGET_INDEX: u32 = 19;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let db_path = ".bdk_kyoto_wallet_example";
-    let mut db = Store::<wallet::ChangeSet>::open_or_create_new(b"db_magic", db_path)?;
-    let loaded = db.aggregate_changesets()?;
-
     let desc = "tr([7d94197e/86'/1'/0']tpubDCyQVJj8KzjiQsFjmb3KwECVXPvMwvAxxZGCP9XmWSopmjW3bCV3wD7TgxrUhiGSueDS1MU5X1Vb1YjYcp8jitXc5fXfdC1z68hDDEyKRNr/0/*)";
     let change_desc = "tr([7d94197e/86'/1'/0']tpubDCyQVJj8KzjiQsFjmb3KwECVXPvMwvAxxZGCP9XmWSopmjW3bCV3wD7TgxrUhiGSueDS1MU5X1Vb1YjYcp8jitXc5fXfdC1z68hDDEyKRNr/1/*)";
 
-    let mut wallet = Wallet::new_or_load(desc, change_desc, loaded, Network::Signet)?;
-    println!(
-        "{}",
-        wallet.next_unused_address(KeychainKind::External).address
-    );
+    let mut wallet = Wallet::new(desc, change_desc, Network::Signet)?;
+
     println!(
         "Balance before sync: {} sats",
         wallet.balance().total().to_sat()
     );
 
-    // Get revealed script pubkeys for each keychain, defaulting to 20 if none are revealed
+    // Populate the list of watched script pubkeys
     let mut spks: HashSet<ScriptBuf> = HashSet::new();
     for keychain in [KeychainKind::External, KeychainKind::Internal] {
-        let last_reveal = match wallet.derivation_index(keychain) {
-            Some(index) => index,
-            None => {
-                let target_index = 19;
-                let _ = wallet.reveal_addresses_to(keychain, target_index);
-                target_index
-            }
-        };
-        for idx in 0..=last_reveal {
-            let spk = wallet.spk_index().spk_at_index(keychain, idx).unwrap();
-            spks.insert(spk.to_owned());
+        for index in 0..=TARGET_INDEX {
+            spks.insert(wallet.peek_address(keychain, index).script_pubkey());
         }
     }
 
-    // Get local chain tip
-    let tip = wallet.latest_checkpoint().block_id();
-    println!("Last local tip: {} {}", tip.height, tip.hash);
-
-    let header_cp = HeaderCheckpoint::new(tip.height, tip.hash);
-    // let header_cp = HeaderCheckpoint::new(
-    //     169_000,
-    //     BlockHash::from_str("000000ed6fe89c46140f55ff511c558bcbdb1239ba95474f38f619b3bb657d4a")?,
-    // );
+    // Set a wallet birthday
+    let header_cp = HeaderCheckpoint::new(
+        169_000,
+        BlockHash::from_str("000000ed6fe89c46140f55ff511c558bcbdb1239ba95474f38f619b3bb657d4a")?,
+    );
 
     // Configure kyoto node and console logger
     let subscriber = tracing_subscriber::FmtSubscriber::new();
@@ -89,9 +71,7 @@ async fn main() -> anyhow::Result<()> {
         tokio::task::spawn(async move { node.run().await });
     }
 
-    println!("Syncing!");
-
-    // Apply updates
+    // Sync and apply updates
     if let Some(update) = client.sync().await {
         let bdk_kyoto::Update {
             cp,
@@ -107,22 +87,18 @@ async fn main() -> anyhow::Result<()> {
 
     let _ = client.shutdown().await?;
 
-    // Persist changes
-    if let Some(changeset) = wallet.take_staged() {
-        db.append_changeset(&changeset)?;
-    }
-
     let cp = wallet.latest_checkpoint();
     println!("Synced to tip: {} {}", cp.height(), cp.hash());
     println!("Tx count: {}", wallet.transactions().count());
     println!("Balance: {:#?}", wallet.balance());
-    for keychain in [KeychainKind::External, KeychainKind::Internal] {
-        println!(
-            "Last revealed: {:?} {}",
-            keychain,
-            wallet.derivation_index(keychain).unwrap()
-        );
-    }
+    println!(
+        "Last revealed External: {}",
+        wallet.derivation_index(KeychainKind::External).unwrap()
+    );
+    println!(
+        "Last revealed Internal: {}",
+        wallet.derivation_index(KeychainKind::Internal).unwrap()
+    );
 
     Ok(())
 }
